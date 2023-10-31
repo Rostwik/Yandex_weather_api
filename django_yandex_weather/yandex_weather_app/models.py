35908e7356fd15e7cfcc3f7b71bab66634d05f86
rostw <rostwik@gmail.com>
2023-11-01 00:40:10 +0300

    Add command load_towns

--- django_yandex_weather/yandex_weather_app/models.py
+++ django_yandex_weather/yandex_weather_app/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Town(models.Model):
-    Name = models.CharField(max_length=200, unique=True, verbose_name='Название города')
-    lon = models.FloatField(verbose_name='Долгота')
+    name = models.CharField(max_length=200, verbose_name='Название города')
     lat = models.FloatField(verbose_name='Широта')
+    lon = models.FloatField(verbose_name='Долгота')
     temp = models.IntegerField(verbose_name='Температура', blank=True, null=True)
     wind_speed = models.IntegerField(verbose_name='Скорость ветра', blank=True, null=True)
     pressure_mm = models.IntegerField(verbose_name='Давление', blank=True, null=True)
@@ -12,4 +12,4 @@ class Town(models.Model):
     is_request = models.BooleanField(verbose_name='Наличие запроса', blank=True, null=True)
 
     def __str__(self):
-        return self.Name
+        return self.name
