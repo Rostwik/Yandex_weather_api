ab2fd0ff7ab9195088d1163ea2c36efa4509bf56
rostw <rostwik@gmail.com>
2024-01-15 22:29:00 +0300

    Refactoring

--- django_yandex_weather/yandex_weather_app/urls.py
+++ django_yandex_weather/yandex_weather_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.shortcuts import render
 
 from yandex_weather_app import views
 
 urlpatterns = [
     path('weather/', views.weather, name='weather'),
-    path('sun/', views.weather, name='sun'),
+    path('sun/', render, kwargs={'template_name': 'index.html'}, name='sun_page'),
 ]
