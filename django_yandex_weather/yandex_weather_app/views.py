455cb9a6a9408f49f736994e852633332dd5826c
rostw <rostwik@gmail.com>
2024-01-15 23:36:16 +0300

    Refactoring

--- django_yandex_weather/yandex_weather_app/views.py
+++ django_yandex_weather/yandex_weather_app/views.py
@@ -1,6 +1,7 @@
 import pytz
 import requests
 from django.http import JsonResponse
+from django.shortcuts import render
 from django.utils import timezone
 
 from environs import Env
@@ -15,45 +16,47 @@ yandex_weather_key = env.str('YANDEX_WEATHER_API_KEY')
 
 
 def sun(request):
-    try:
-        if request.method == 'GET':
-            town = {
-                'name': 'СПб',
-                'lat': 59.94,
-                'lon': 30.31
-            }
-            yandex_api_url = 'https://api.weather.yandex.ru/v2/informers'
-            headers = {
-                'X-Yandex-API-Key': yandex_weather_key
-            }
-            payload = {
-                'lat': town['lat'],
-                'lon': town['lon'],
-                'extra': 'false'
-            }
-            response = requests.get(yandex_api_url, params=payload, headers=headers)
-            print(response)
-            response.raise_for_status()
-            weather_fact = response.json()['fact']
-            print(weather_fact)
-            weather_forecast = response.json()['forecast']
-            print(weather_forecast)
+    return render(request, "index.html")
 
-            return JsonResponse(
-                {
-                    'Город': town['name'],
-                    'Температура': f"{weather_fact['temp']} градусов Цельсия",
-                    'Атмосферное давление': f"{weather_fact['pressure_mm']} мм рт.ст.",
-                    'Скорость ветра': f"{weather_fact['wind_speed']} м/с",
-                    'sunrise': weather_forecast['sunrise'],
-                    'sunset': weather_forecast['sunset']
-                }
-                , safe=False, json_dumps_params={
-                    'ensure_ascii': False,
-                    'indent': 4,
-                })
-    except Exception as exp:
-        print(exp)
+    # try:
+    #     if request.method == 'GET':
+    #         town = {
+    #             'name': 'СПб',
+    #             'lat': 59.94,
+    #             'lon': 30.31
+    #         }
+    #         yandex_api_url = 'https://api.weather.yandex.ru/v2/informers'
+    #         headers = {
+    #             'X-Yandex-API-Key': yandex_weather_key
+    #         }
+    #         payload = {
+    #             'lat': town['lat'],
+    #             'lon': town['lon'],
+    #             'extra': 'false'
+    #         }
+    #         response = requests.get(yandex_api_url, params=payload, headers=headers)
+    #         print(response)
+    #         response.raise_for_status()
+    #         weather_fact = response.json()['fact']
+    #         print(weather_fact)
+    #         weather_forecast = response.json()['forecast']
+    #         print(weather_forecast)
+    #
+    #         return JsonResponse(
+    #             {
+    #                 'Город': town['name'],
+    #                 'Температура': f"{weather_fact['temp']} градусов Цельсия",
+    #                 'Атмосферное давление': f"{weather_fact['pressure_mm']} мм рт.ст.",
+    #                 'Скорость ветра': f"{weather_fact['wind_speed']} м/с",
+    #                 'sunrise': weather_forecast['sunrise'],
+    #                 'sunset': weather_forecast['sunset']
+    #             }
+    #             , safe=False, json_dumps_params={
+    #                 'ensure_ascii': False,
+    #                 'indent': 4,
+    #             })
+    # except Exception as exp:
+    #     print(exp)
 
 def weather(request):
     if request.method == 'GET':
